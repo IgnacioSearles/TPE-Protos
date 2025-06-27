6c072c43819b0b4ad8bf5bee092bfda6169db778
Ignacio Searles <nachosearles3@gmail.com>
2025-06-26 21:19:51 -0300

    chore: format code

--- src/netutils.c
+++ src/netutils.c
@@ -1,19 +1,18 @@
-#include <stdbool.h>
 #include <errno.h>
-#include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #include "netutils.h"
 
-#define N(x) (sizeof(x)/sizeof((x)[0]))
+#define N(x) (sizeof(x) / sizeof((x)[0]))
 
-extern const char *
-sockaddr_to_human(char *buff, const size_t buffsize,
-                  const struct sockaddr *addr) {
-    if(addr == 0) {
+extern const char *sockaddr_to_human(char *buff, const size_t buffsize,
+                                     const struct sockaddr *addr) {
+    if (addr == 0) {
         strncpy(buff, "null", buffsize);
         return buff;
     }
@@ -21,20 +20,20 @@ sockaddr_to_human(char *buff, const size_t buffsize,
     void *p = 0x00;
     bool handled = false;
 
-    switch(addr->sa_family) {
-        case AF_INET:
-            p    = &((struct sockaddr_in *) addr)->sin_addr;
-            port =  ((struct sockaddr_in *) addr)->sin_port;
-            handled = true;
-            break;
-        case AF_INET6:
-            p    = &((struct sockaddr_in6 *) addr)->sin6_addr;
-            port =  ((struct sockaddr_in6 *) addr)->sin6_port;
-            handled = true;
-            break;
+    switch (addr->sa_family) {
+    case AF_INET:
+        p = &((struct sockaddr_in *)addr)->sin_addr;
+        port = ((struct sockaddr_in *)addr)->sin_port;
+        handled = true;
+        break;
+    case AF_INET6:
+        p = &((struct sockaddr_in6 *)addr)->sin6_addr;
+        port = ((struct sockaddr_in6 *)addr)->sin6_port;
+        handled = true;
+        break;
     }
-    if(handled) {
-        if (inet_ntop(addr->sa_family, p,  buff, buffsize) == 0) {
+    if (handled) {
+        if (inet_ntop(addr->sa_family, p, buff, buffsize) == 0) {
             strncpy(buff, "unknown ip", buffsize);
             buff[buffsize - 1] = 0;
         }
@@ -46,7 +45,7 @@ sockaddr_to_human(char *buff, const size_t buffsize,
     buff[buffsize - 1] = 0;
     const size_t len = strlen(buff);
 
-    if(handled) {
+    if (handled) {
         snprintf(buff + len, buffsize - len, "%d", ntohs(port));
     }
     buff[buffsize - 1] = 0;
@@ -54,12 +53,11 @@ sockaddr_to_human(char *buff, const size_t buffsize,
     return buff;
 }
 
-int
-sock_blocking_write(const int fd, buffer *b) {
-        int  ret = 0;
-    ssize_t  nwritten;
-	 size_t  n;
-	uint8_t *ptr;
+int sock_blocking_write(const int fd, buffer *b) {
+    int ret = 0;
+    ssize_t nwritten;
+    size_t n;
+    uint8_t *ptr;
 
     do {
         ptr = buffer_read_ptr(b, &n);
@@ -75,13 +73,12 @@ sock_blocking_write(const int fd, buffer *b) {
     return ret;
 }
 
-int
-sock_blocking_copy(const int source, const int dest) {
+int sock_blocking_copy(const int source, const int dest) {
     int ret = 0;
     char buf[4096];
     ssize_t nread;
     while ((nread = recv(source, buf, N(buf), 0)) > 0) {
-        char* out_ptr = buf;
+        char *out_ptr = buf;
         ssize_t nwritten;
         do {
             nwritten = send(dest, out_ptr, nread, MSG_NOSIGNAL);
@@ -94,8 +91,7 @@ sock_blocking_copy(const int source, const int dest) {
             }
         } while (nread > 0);
     }
-    error:
+error:
 
     return ret;
 }
-
